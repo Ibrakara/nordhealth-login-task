325f4ea7aef99e2dcf47bf00195e81edd4ee3cf0
Ibrahim Karaduman <ibr.karaduman@gmail.com>
2025-05-12 19:47:02 +0300

    Add success page component with authentication middleware

---
+++ pages/success.vue
@@ -0,0 +1,32 @@
+<template>
+  <provet-stack
+    class="success-page-container"
+    justify-content="center"
+    align-items="center"
+  >
+    <provet-card padding="l">
+      <h1 slot="header">Success!</h1>
+      <p>You have successfully signed up for the product.</p>
+      <provet-button @click="$router.push('/')">
+        <provet-icon slot="start" name="arrow-left-long"></provet-icon>
+        Back
+      </provet-button>
+    </provet-card>
+  </provet-stack>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["auth"],
+});
+useHead({
+  title: "Success",
+});
+</script>
+
+<style scoped>
+.success-page-container {
+  max-width: 340px;
+  margin: var(--n-space-xl) auto;
+  text-align: center;
+}
+</style>
